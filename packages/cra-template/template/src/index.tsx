5c7376d079c2fdb96e874b69f079bb465ab43c7a
vinciusb <65973642+vinciusb@users.noreply.github.com>
2021-12-26 11:48:13 -0300

    First

--- packages/cra-template/template/src/index.tsx
+++ packages/cra-template/template/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './ReactUtils/index.css';
-import App from './Components/App/App';
-import reportWebVitals from './ReactUtils/reportWebVitals';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
